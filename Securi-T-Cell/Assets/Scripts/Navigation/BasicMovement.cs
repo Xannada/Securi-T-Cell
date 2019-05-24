05a212cd3887b9a9d688cffb111f1b61bff8f860
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-05-24 16:19:04 -0700

    fixed little mistakes

--- Securi-T-Cell/Assets/Scripts/Navigation/BasicMovement.cs
+++ Securi-T-Cell/Assets/Scripts/Navigation/BasicMovement.cs
@@ -10,17 +10,14 @@ public class BasicMovement : MonoBehaviour
 
     public float speed;
 
-    // Start is called before the first frame update
     void Start()
     {
         nma = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector2 movementThisFrame = (new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"))).normalized;
-        Debug.Log(movementThisFrame);
         nma.Move(new Vector3(movementThisFrame.x, 0f, movementThisFrame.y) * speed * Time.deltaTime);
     }
 }
