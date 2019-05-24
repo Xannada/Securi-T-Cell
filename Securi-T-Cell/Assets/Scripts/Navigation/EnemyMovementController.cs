824eabcd39bf8defc95c2c575ce2b734a478d758
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-05-24 16:09:41 -0700

    Add basic NavMesh implementation

---
+++ Securi-T-Cell/Assets/Scripts/Navigation/EnemyMovementController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovementController : MonoBehaviour
+{
+    private NavMeshAgent nma;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        nma = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nma.SetDestination(player.transform.position);
+    }
+}
