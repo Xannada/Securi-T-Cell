636b6af8c0711d8645b909e2eb79e6f7c7be5d44
Justin Kephart <justinpianist1@yahoo.com>
2019-06-03 02:56:22 -0700

    Large Enemies, Disable mechanic, lowered health

--- Securi-T-Cell/Assets/Scripts/PlayerEating.cs
+++ Securi-T-Cell/Assets/Scripts/PlayerEating.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerEating : MonoBehaviour
 {
@@ -66,10 +67,20 @@ public class PlayerEating : MonoBehaviour
         Debug.Log("Collision");
         if (col.transform.CompareTag("Enemy"))
         {
-            if (col.transform.GetComponent<EnemyStats>().readyToDie)
+            EnemyStats target = col.transform.GetComponent<EnemyStats>();
+
+            if (target.readyToDie)
             {
-                Destroy(col.transform.gameObject);
-                Eat();
+                
+                {
+                    Destroy(col.transform.gameObject);
+                    if (!target.large) Eat();
+                    else
+                    {
+                        Instantiate(Resources.Load("Chunks"), target.transform.position, Quaternion.identity);
+                        Destroy(col.transform.gameObject);
+                    }
+                }
             }
         }
     }
