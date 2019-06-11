96caa9bc5188b67101ef5e82f4389aec6baa9001
Parker Grade <pg.elvandar@gmail.com>
2019-06-11 13:26:58 -0700

    Adjusted UI, added health bar
    -Adjusted what the top of the UI looks like
    -added a health bar that adjusts to current health

--- Securi-T-Cell/Assets/Scripts/UI/HealthUpdater.cs
+++ Securi-T-Cell/Assets/Scripts/UI/HealthUpdater.cs
@@ -7,12 +7,15 @@ using UnityEngine.UI;
 public class HealthUpdater : MonoBehaviour
 {
     public int delayedUpdateAmount;//runs update once every n frames whre this is n
+    public GameObject bar;
+    private RectTransform rt;
     
     private Text counter;
     // Start is called before the first frame update
     void Start()
     {
         counter = GetComponent<Text>();
+        rt = bar.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -20,5 +23,6 @@ public class HealthUpdater : MonoBehaviour
     {
         if (Time.frameCount % delayedUpdateAmount != 0) return;//do nothing and exit if not yet time to update.
         counter.text = "" + PlayerStats.player.health;
+        rt.sizeDelta = new Vector2((PlayerStats.player.health / 10f)* 200f, 40);
     }
 }
