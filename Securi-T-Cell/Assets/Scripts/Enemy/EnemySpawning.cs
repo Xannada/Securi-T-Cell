da819f93b2cbe59d133bfe2192637d5c3650a697
Justin Kephart <justinpianist1@yahoo.com>
2019-06-11 15:24:45 -0700

    Limited maximum amount of small enemies to 300

--- Securi-T-Cell/Assets/Scripts/Enemy/EnemySpawning.cs
+++ Securi-T-Cell/Assets/Scripts/Enemy/EnemySpawning.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class EnemySpawning : MonoBehaviour
 {
     private float timer = 0;
+    private static GameObject spawn;
     private float spawnTime = 10;
-    private float spawnCount = 3;
+    private int spawnCount = 3;
+    private int spawnLimit = 300;
     public GameObject enemy;
 
     private void Awake()
     {
         if (!enemy) enemy = Resources.Load("Enemy2D") as GameObject;
+    }
+
+    private void Start()
+    {
+        if (!spawn)
+        {
+            if (GameObject.Find("Small Enemies")) spawn = GameObject.Find("Small Enemies");
+            else spawn = Instantiate(new GameObject("Small Enemies"));
+        }
     }
 
     // Update is called once per frame
@@ -20,8 +31,11 @@ public class EnemySpawning : MonoBehaviour
         timer += Time.deltaTime;
         if (timer > spawnTime)
         {
-            for (int i = 0; i < spawnCount; i++)
-                Instantiate(enemy, transform.position, Quaternion.identity);
+            int limit = Mathf.Min(spawnCount, spawnLimit - spawn.transform.childCount + spawnCount);
+            for (int i = 0; i < limit; i++)
+            {
+                Instantiate(enemy, transform.position, Quaternion.identity, spawn.transform);
+            }
             timer = 0;
         }
     }
