2999b8f962e271af4a158aa6c740029b3a9990cd
Parker Grade <pg.elvandar@gmail.com>
2019-06-11 14:20:02 -0700

    Fixed the thermometer

--- Securi-T-Cell/Assets/Scripts/UI/ThermometerUpdater.cs
+++ Securi-T-Cell/Assets/Scripts/UI/ThermometerUpdater.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class ThermometerUpdater : MonoBehaviour
 {
-    public Vector2 heightRange, yPosRange;
+    //public Vector2 heightRange, yPosRange;
+    public GameObject bar;
     public int delayedUpdateAmount;//runs update once every n frames whre this is n
     
     private GameObject EnemyParent;
@@ -22,8 +23,10 @@ public class ThermometerUpdater : MonoBehaviour
     void Update()
     {
         if (Time.frameCount % delayedUpdateAmount != 0) return;//do nothing and exit if not yet time to update.
-        float percentToEnd = EnemyParent.transform.childCount * 1.0f / 100f;
-        Fill.rectTransform.position = 
+        float percentToEnd = EnemyParent.transform.childCount / 100f;
+        bar.GetComponent<RectTransform>().sizeDelta = new Vector2(20, percentToEnd * 200f);
+
+        /*Fill.rectTransform.position = 
             new Vector3(
                 Fill.rectTransform.position.x,
                 Mathf.Lerp(yPosRange[0], yPosRange[1], percentToEnd),
@@ -33,6 +36,6 @@ public class ThermometerUpdater : MonoBehaviour
             new Vector2(
                 Fill.rectTransform.sizeDelta.x,
                 Mathf.Lerp(heightRange[0], heightRange[1], percentToEnd)
-                );
+                );*/
     }
 }
